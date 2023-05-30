5e0190a27af01790f9cc1a39601c0bdff04021e9
anUnknownStoic <104714642+benbiji63@users.noreply.github.com>
2023-05-30 16:18:39 +0530

    Completed the A wolf in sheep's clothing problem and add an  some other solutions for Count the Monkeys problem

--- countTheMonkeys/OtherSolutions.js
+++ countTheMonkeys/OtherSolutions.js
@@ -1,9 +1,9 @@
 function monkeyCount(n) {
-  return Array.from({length:n}, (_,i)=>i+1)
+  return Array.from({ length: n }, (_, i) => i + 1);
 }
 
 function monkeyCount(n) {
-  return [...Array(n+1).keys()].slice(1);
+  return [...Array(n + 1).keys()].slice(1);
 }
 
-var monkeyCount=n=>[...Array(n)].map((v,i)=>i+1)
+var monkeyCount = (n) => [...Array(n)].map((v, i) => i + 1);
