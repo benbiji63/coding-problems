e3006111416855b2c15f4e4fda49879fa6d4e653
anUnknownStoic <104714642+benbiji63@users.noreply.github.com>
2023-07-05 10:38:09 +0530

    Completed honeyToTheBee problem

---
+++ honeyToTheBee/answer.js
@@ -0,0 +1,45 @@
+// Initial code to be debugged
+function Bee(capacity, hive) {
+  this.capacity = capacity;
+  this.hive = hive;
+}
+
+function Hive() {
+  this.pollen = 100;
+}
+
+//TODO: The amount of honey in the hive is not increasing.
+Hive.prototype.addPollen = function(pollen) {
+  this.pollen += pollen;
+}
+
+Hive.prototype.getPollen = function() {
+  return this.pollen;
+}
+
+Bee.prototype.unloadPollen = function() {
+  hive.addPollen(this.capacity);
+}
+// Solution 
+// unloadPollen on function should run on thishive
+function Bee(capacity, hive) {
+  this.capacity = capacity;
+  this.hive = hive;
+}
+
+function Hive() {
+  this.pollen = 100;
+}
+
+// TODO: The amount of honey in the hive is not increasing.
+Hive.prototype.addPollen = function(pollen) {
+  this.pollen += pollen;
+}
+
+Hive.prototype.getPollen = function() {
+  return this.pollen;
+}
+
+Bee.prototype.unloadPollen = function() {
+  this.hive.addPollen(this.capacity);
+}
