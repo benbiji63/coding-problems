5caed1eddc81b1e6b38a58fef84b2a3bb0f24d09
anUnknownStoic <104714642+benbiji63@users.noreply.github.com>
2023-08-10 20:13:56 +0530

    Solved emptyObject challenge

---
+++ emptyObj/answer.js
@@ -0,0 +1,5 @@
+// An object
+// Return true .if it's empty else return false
+// Take all the keys of the object to an array and check its length
+
+const isEmpty = obj => Object.keys(obj).length === 0;
